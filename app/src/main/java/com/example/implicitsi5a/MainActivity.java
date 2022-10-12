46eedd1a2fa5f3d9d8718ccaef643ba5b30c0e9c
NameisKey <106938020+NameisKey@users.noreply.github.com>
2022-10-12 17:02:08 +0700

    baru url

--- app/src/main/java/com/example/implicitsi5a/MainActivity.java
+++ app/src/main/java/com/example/implicitsi5a/MainActivity.java
@@ -1,14 +1,59 @@
-package com.example.implicitsi5a;
+    package com.example.implicitsi5a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.URI;
+
+    public class MainActivity extends AppCompatActivity {
+    EditText etUrl,etLokasi,etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
+    String getWebsite,getLokasi, getTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUrl = findViewById(R.id.et_url);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_text);
+        btnBukaWebsite = findViewById(R.id.btn_buka_website);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWebsite = etUrl.getText().toString();
+                Uri urigetWebsite = Uri.parse(getWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, urigetWebsite);
+                try {
+                    startActivity(intentWebsite);
+                }catch (Exception e){
+                    etUrl.setError("Url Tidak Sesuai!");
+                }
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
